18aad4758b53ac681fe5a7ea0f556f2271abf215
mndowne <mndowne@gmail.com>
2017-02-08 18:36:54 -0600

    just getting more set up before adding content

---
+++ src/7-1-General_Concepts_and_Criteria.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+require('./css/section.css');
+
+
+class SevenOne extends React.Component {
+    constructor(props)  {
+        super(props);
+
+    }
+
+    render()  {
+        return(
+            <div className='section'>
+                Seven dash one
+            </div>
+        );
+    }
+}
+
+module.exports = SevenOne;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SevenOne from './7-1-General_Concepts_and_Criteria.js';
 
+
+require('./css/style.css');
+
+
+class App extends React.Component {
+    constructor(props)  {
+        super(props);
+
+    }
+
+    render()  {
+        return(
+            <div>
+                Hello World
+                <SevenOne />
+            </div>
+        );
+    }
+}
+
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<App />, app );
